d49aef7a38e16ec845d8fd513082fe16c80f3127
asmaa kamal <asma2_kamal2009@yahoo.com>
2020-04-21 05:10:21 +0200

    Updates

---
+++ precache-manifest.0ca2d117afbb4ed0c0ad06bba8e0d73e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b220316e5c6cc3ac8473322afeb709",
+    "url": "/reactjs-weather-app/index.html"
+  },
+  {
+    "revision": "bfb99d8069f22eae9d3e",
+    "url": "/reactjs-weather-app/static/css/main.e81a30d7.chunk.css"
+  },
+  {
+    "revision": "49acb864f6bb32bd5863",
+    "url": "/reactjs-weather-app/static/js/2.e5e02872.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-weather-app/static/js/2.e5e02872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb99d8069f22eae9d3e",
+    "url": "/reactjs-weather-app/static/js/main.7e01e4a6.chunk.js"
+  },
+  {
+    "revision": "7567c1f3c9f0cba092f7",
+    "url": "/reactjs-weather-app/static/js/runtime-main.2c8f4dc7.js"
+  }
+]);
